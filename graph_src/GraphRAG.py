4a5c83ef76cbdb562bd29f863ee3c1be11a45261
Marco <91677804+MarcoCSL@users.noreply.github.com>
2026-03-04 14:08:47 +0100

    Refactoring

--- graph_src/GraphRAG.py
+++ graph_src/GraphRAG.py
@@ -12,23 +12,23 @@ class GraphRAG():
 		self.entity_linker = EntityLinker(device)
 		self.db_interviewer = DBInterviewer()
 
-	# rimetti paths_for_pair a 2
 	def search(self, question, options, max_num_pairs=10, paths_for_pair=2, retrieve_definitions=True):
 		query = self.input_formatter(question, options)
 		try:
 			entities = self.ner.find(query)
 
-			linked_entities = []
+			linked_entities = set()
 			for entity in entities:
-				result = self.entity_linker.link(entity)
-				
-				if isinstance(result, list) and all(isinstance(r, list) for r in result):
-					linked_entities.extend(result) # If it has duplicates add them both, es:
-					# [[127478, 'exposure', 'Phenol'], [17377, 'drug', 'Phenol']]
-				else:
-					linked_entities.append(result)
+				link_result = self.entity_linker.link(entity)
 
-			linked_entities = list({tuple(e) for e in linked_entities})
+				if isinstance(link_result, list) and all(isinstance(r, list) for r in link_result):
+					for r in link_result:
+						linked_entities.add(tuple(r)) # If it has duplicates add them both, es:
+						# [[127478, 'exposure', 'Phenol'], [17377, 'drug', 'Phenol']]
+				else:
+					linked_entities.add(tuple(link_result))
+
+			linked_entities = list(linked_entities)
 			pairs = list(combinations(linked_entities, 2))
 			pairs = [(a, b) for (a, b) in pairs if a[2] != b[2]]
 
@@ -36,7 +36,6 @@ class GraphRAG():
 				pairs = random.sample(pairs, max_num_pairs)
 
 			contexts = [self.db_interviewer.Yen(source, target, paths_for_pair) for source, target in pairs]
-
 			context = self.context_cleaner(contexts, retrieve_definitions)
 
 			return context, self.num_paths(contexts)
